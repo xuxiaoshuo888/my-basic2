9bbe7da2f3c00f85f2cf4f81bf7e7c5aadf9e291
xus <754124808@qq.com>
2018-09-21 19:07:08 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+/* 国际化 */
+import en from 'element-ui/lib/locale/lang/en';
+import ElementUI from 'element-ui';
 import App from './App';
 import router from './router';
-import ElementUI from "element-ui";
 
-/* 国际化 */
-import en from "element-ui/"
-
-Vue.use(ElementUI,{local})
+Vue.use(ElementUI, { en });
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
